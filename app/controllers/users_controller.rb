aca0b61bc47ecc79fd132b88f129be7c199a9120
austin shields <ajshields196@gmail.com>
2021-05-31 02:10:59 -0400

    add users show route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,29 @@
 class UsersController < ApplicationController
     skip_before_action :authorize_request, only: :create
+
     def create
       user = User.create!(user_params)
       auth_token = AuthenticateUser.new(user.email, user.password).call
       response = { message: Message.account_created, auth_token: auth_token }
       
       json_response(response, :created)
+    end
+
+    def show
+      if @current_user.id == params[:id].to_i
+        user = User.find(params[:id])
+        response = {
+          name: user.name,
+          email: user.email,
+          id: user.id,
+          created_at: user.created_at,
+          updated_at: user.updated_at,
+        }
+      else
+        response = { message: Message.unauthorized }
+      end
+      
+      json_response(response)
     end
   
     private
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
 
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
+  get 'users/:id', to: 'users#show'
 end
